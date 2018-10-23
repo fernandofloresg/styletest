828695d71d46f387a3bc531c1778506c863f54dc
Humberto <bethoeazy@gmail.com>
2018-10-23 13:46:27 -0700

    estilo de las graficas

--- chart_logic.js
+++ chart_logic.js
@@ -5,32 +5,36 @@ var myChart = new Chart(ctx, {
         labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
         datasets: [{
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 10
+            data: [12, 19, 3, 12, 5, 15, 7, 2, 10],
+            backgroundColor: '#FFF',
+            borderColor: '#FFF',
+            borderWidth: 10,
+            fill: false
         }]
     },
     options: {
+      responsive: true,
       elements: {
             line: {
                 tension: 0, // disables bezier curves
             }
-        }
+        },
+        scales: {
+          xAxes: [{
+            display: false,
+          }],
+          yAxes: [{
+            display:false,
+          }]
+        },
+        hover: {
+          mode: 'point',
+          intersect: true
+        },
+        tooltips: {
+          mode: 'index',
+          intersect: false,
+        },
     }
 });
 
